2cf2759e41117e150689d0a85ea1335fb65ac288
zhuo <zhuo_2003@outlook.com>
2026-09-03 21:38:09 +0800

    mini_ai_runtime : test cuda_tensor

---
+++ test/tensor/test_cuda_tensor.cu
@@ -0,0 +1,30 @@
+#include <cassert>
+#include <iostream>
+
+#include <cuda_runtime>
+
+#include "runtime/tensor/tensor.h"
+
+using namespace runtime;
+
+void testCUDATensor();
+
+int main(){
+
+    testCUDATensor();
+
+}
+
+void testCUDATensor(){
+
+    Tensor A(Shape({1024}), DataType::Int32, DeviceType::CUDA);
+
+    assert(A.rank() == 2);
+    assert(A.device() == DeviceType::CUDA);
+    assert(A.dtype() == DataType::Int32);
+    assert(A.bytes() == 1024 * sizeof(int));
+    assert(A.empty() == false);
+
+    std::cout << "[PASS] CUDA Tensor" << std::endl;
+
+}
